41cff27df8c6db4cee4aaada8c934eb9df9014e1
Daniel <danielmiller20@gmail.com>
2017-03-02 17:52:10 +0200

    Project initialized.

--- BE/Properties/AssemblyInfo.cs
+++ BE/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RssReader")]
+[assembly: AssemblyTitle("BE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RssReader")]
+[assembly: AssemblyProduct("BE")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
